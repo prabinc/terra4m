ce12f834053a9d709535aae1649bbd2efe9cc6a4
__prabin <16086435+prabinc@users.noreply.github.com>
2021-04-06 19:35:13 +0000

    Update test.py

--- test.py
+++ test.py
@@ -13,10 +13,12 @@ print(type(certs))
 def check_cert_format(certs):
   print("entering function")
   for item in certs:
-    cert = ast.literal_eval(item)
-    
     try:
-      isinstance(cert, dict)
+      cert = ast.literal_eval(item)
+    except:
+      raise ValueError("Enter a valid cert")
+    try:
+      isinstance(item, dict)
     except:
       print("Please input cert in a valid format.")
       
