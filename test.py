da82a93f40ef688840a2d8ebf96845ea9dd02dff
__prabin <16086435+prabinc@users.noreply.github.com>
2021-04-06 10:36:25 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import os
+import re
 import json
 from collections import OrderedDict
 
 print("hello world, for real?")
 print(os.environ['env_name'])
 print(type(os.environ['certs']))
-print(os.environ['certs'])
+certs = os.environ['certs'].splitlines()
+print(certs)
+
+
 
